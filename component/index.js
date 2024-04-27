6090a329cb170a2b849c1d2c2ad473e724d57008
BBCrank3107 <binhnguyenpk24@gmail.com>
2024-04-27 23:00:20 +0700

    chang file

--- component/index.js
+++ component/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {
-    View, Text, SafeAreaView, Image, StyleSheet
+    Image, StyleSheet
 } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
