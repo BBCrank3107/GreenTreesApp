6300be6148f7a21b56ff8d2388a66e56e38f99bc
BBCrank3107 <binhnguyenpk24@gmail.com>
2024-04-27 20:33:40 +0700

    chang name file

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {
+  StyleSheet,
+} from 'react-native';
+
+import AppServer from './studyCode/AppServer';
+
+const App = () => {
+  return (
+    <AppServer/>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    flex: 1,
+
+  },
+});
+
+export default App;
